baed4383ddb79189a167604bd177f5d4ebd02f97
Jaroslav Machovec <jarda.machovec@gmail.com>
2022-09-29 12:45:05 +0200

    001: Spring project initial commit

---
+++ src/main/java/cz/machovec/endpointmonitor/EndpointMonitorApplication.java
@@ -0,0 +1,13 @@
+package cz.machovec.endpointmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndpointMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/machovec/endpointmonitor/EndpointMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.machovec.endpointmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
